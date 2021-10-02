2a14f462393544d8000f0d6fc1a49b8b524c6e02
teemukissa <tremppu@hotmail.com>
2021-10-02 12:52:28 +0300

    chore: things

---
+++ theme/index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Headless template
+ * @subpackage Redirect
+ * @since Redirect 1.1
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Redirecting</title>
+  <meta name="robots" content="noindex, nofollow">
+  <script>
+  // CHANGE ME
+  // window.location.replace("URL");
+  </script>
+</head>
+<body>
+  <main id="main" >
+    <h1>Redirecting</h1>
+  </main>
+</body>
+</html>
